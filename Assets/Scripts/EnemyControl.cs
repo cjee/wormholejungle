ef0da5a42d91cdaf2a9c1f98416bbc63d930eee0
roberts <roberts.dargis@gmail.com>
2015-01-24 08:40:04 +0200

    EnemyRocket

---
+++ Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyControl : AbstractEnemy {
+
+	
+	public float fireRate;
+	public GameObject shot;
+	public Transform shotSpawn;
+	private float nextFire;
+
+	void FixedUpdate()
+	{
+		Shoot ();
+	}
+
+	void Shoot()
+	{
+		if (Time.time > nextFire) 
+		{
+			nextFire=Time.time+fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+}
