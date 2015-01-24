ec54ae05ad5393ff06bf6675a5752fb8f89f161d
roberts <roberts.dargis@gmail.com>
2015-01-24 02:25:21 +0200

    Bounds

--- Assets/Scripts/DestroyByBoundary.cs
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -2,7 +2,24 @@
 using System.Collections;
 
 public class DestroyByBoundary : MonoBehaviour {
+	
+	void Start()
+	{
+		Resize ();
+	}
 
+	void Resize()
+	{
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+		
+		float worldScreenHeight = Camera.main.orthographicSize * 2;
+		float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+		BoxCollider2D col = (BoxCollider2D)collider2D;
+		col.size= new Vector2(
+			worldScreenWidth,
+			worldScreenHeight);
+
+	}
 
 	void OnTriggerExit2D(Collider2D col)
 	{
