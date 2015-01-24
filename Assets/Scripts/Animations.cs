3940c1c260dea28ac6c3a5c97abfefe6de3d610f
roberts <roberts.dargis@gmail.com>
2015-01-24 22:08:57 +0200

    Portal

---
+++ Assets/Scripts/Animations.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animations {
+
+
+	public static void shrinkAndDestroy(GameObject o)
+	{
+		/*
+		Debug.Log(1);
+		float shrinkRate = 1.9f;
+		float shrinkTo = (new Vector3 (0.1f, 0.1f, 0.1f)).sqrMagnitude;
+		//o.rigidbody2D.velocity = Vector2.zero;
+		
+		Debug.Log(o);
+		while(o!=null && o.transform.localScale.sqrMagnitude>shrinkTo )
+		{
+			o.transform.localScale *= shrinkRate*Time.deltaTime;
+			//Debug.Log(o.transform.localScale);
+			yield return new WaitForSeconds(10);
+		}*/
+
+		MonoBehaviour.Destroy (o);
+
+	}
+}
---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+
+	float velocity;
+
+	void Start () {
+		rigidbody2D.velocity = (new Vector2 (-1.0f, 0.0f)) * velocity;
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		collider.gameObject.transform.position = gameObject.transform.position;
+		Animations.shrinkAndDestroy (collider.gameObject);
+		//StartCoroutine(Animations.shrinkAndDestroy(collider.gameObject));
+	}
+
+}
