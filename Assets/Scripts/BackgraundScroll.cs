99239fbcb39baa7a94c06aeac2b6ef45711a9bff
Edgars <edgars@outlook.com>
2015-01-24 03:34:48 +0200

    Scrollable backgroundAdded

---
+++ Assets/Scripts/BackgraundScroll.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgraundScroll : MonoBehaviour {
+
+	public float speed = 0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		renderer.material.mainTextureOffset = new Vector2 (Time.time * speed, 0.0f);
+	}
+}
