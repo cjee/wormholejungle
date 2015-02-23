4c1ce803ab0aff5fc520e891deffc530e263c3d2
roberts <roberts.dargis@gmail.com>
2015-02-23 17:17:12 +0200

    Fixed portal animation

--- Assets/Scripts/Animations.cs
+++ Assets/Scripts/Animations.cs
@@ -4,31 +4,24 @@ using System.Collections;
 public class Animations {
 
 
-	public static IEnumerator shrinkAndDestroy(GameObject o, Vector3 coordinates)
+	public static IEnumerator shrinkAndDestroy(GameObject o, GameObject portal)
 	{
-			o.collider2D.enabled = false;
+		o.collider2D.enabled = false;
 
-		Vector3 deltaMove = coordinates - o.transform.position;
+		Vector3 deltaMove = portal.transform.position - o.transform.position;
 		Vector3 absMove = new Vector3 (Mathf.Abs (deltaMove.x), Mathf.Abs (deltaMove.y));
 		Vector3 moveReminder = absMove;
 		Vector3 direction = new Vector3 ((deltaMove.x < 0 ? -1.0f : 1.0f), (deltaMove.y < 0 ? -1.0f : 1.0f));
 
-		float moveRate = Mathf.Max (absMove.x,absMove.y)/0.2f;
+		float moveRate = Mathf.Max (absMove.x,absMove.y,10)/0.2f;
 
-		while(o!=null && moveReminder.x>0.0f && moveReminder.y>0.0f )
+		while(o!=null && Vector2.Distance(o.transform.position,portal.transform.position)>0.2f )
 		{
-
-			Vector3 move = absMove*moveRate*Time.deltaTime;
-			Vector3 clamp_move = new Vector3(
-				Mathf.Clamp(move.x,0.0f,moveReminder.x),
-				Mathf.Clamp(move.y,0.0f,moveReminder.y)
-			);
-
-			o.transform.position+=new Vector3(clamp_move.x*direction.x,clamp_move.y*direction.y);
-			moveReminder-=clamp_move;
+			o.rigidbody2D.velocity = portal.rigidbody2D.velocity;
+			o.transform.position=Vector2.MoveTowards(o.transform.position,portal.transform.position,moveRate*Time.deltaTime);
+			o.transform.localPosition -= Vector3.forward;
 			yield return new WaitForSeconds(0.03f);
 		}
-
 		float shrinkRate=0.0f;
 		if (o != null)
 			shrinkRate = Mathf.Min (o.transform.localScale.x,o.transform.localScale.y)/0.2f;
--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -7,15 +7,15 @@ public class Portal : MonoBehaviour {
 
 	void Start () {
 		rigidbody2D.velocity = (new Vector2 (-1.0f, 0.0f)) * velocity;
+
 	}
 
 	void OnCollisionEnter2D(Collision2D collider)
 	{
-		
-		if (collider.rigidbody == null)
-						return;
-		collider.gameObject.rigidbody2D.velocity = rigidbody2D.velocity;
-		StartCoroutine(Animations.shrinkAndDestroy(collider.gameObject,gameObject.transform.position));
+		/*
+		if (collider.gameObject.rigidbody2D == null)
+						return;*/
+		StartCoroutine(Animations.shrinkAndDestroy(collider.gameObject,gameObject));
 		
 	}
 
